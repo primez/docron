6222421994fcf4b7668c644a3208623e4d88274a
Vlad Leonov <vleonov@planatechnologies.com>
2024-10-09 12:30:44 +0300

    Initial setup

---
+++ src/Docron.TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+var connection = Environment.GetEnvironmentVariable("Connection");
+
+Console.WriteLine($"Using the connection: {connection}");
+
+var client = string.IsNullOrEmpty(connection)
+    ? new DockerClientConfiguration().CreateClient()
+    : new DockerClientConfiguration(new Uri(connection)).CreateClient();
+
+var containers = await client.Containers
+    .ListContainersAsync(new ContainersListParameters
+    {
+        All = true,
+        Limit = 50,
+    });
+
+foreach (var container in containers)
+{
+    Console.WriteLine(string.Join("; ", container.Names));
+}
+
